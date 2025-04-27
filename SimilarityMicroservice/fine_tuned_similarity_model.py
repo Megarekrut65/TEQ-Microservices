f64717667180f7db5f6fc6b1df610a8509cbef3e
Megarekrut65 <bandalak28@gmail.com>
2025-04-27 19:10:13 +0300

    Update fine_tuned_similarity_model.py

--- SimilarityMicroservice/fine_tuned_similarity_model.py
+++ SimilarityMicroservice/fine_tuned_similarity_model.py
@@ -1,6 +1,7 @@
 from sentence_transformers import SentenceTransformer, util
 
 model = SentenceTransformer("Models/fine_tuned_similarity_model")
+# download from https://github.com/Megarekrut65/TEQ-Microservices/raw/refs/heads/main/SimilarityMicroservice/Models/fine_tuned_similarity_model/model.safetensors
 
 def get_similarity(text1:str, text2:str)->float:
     embeddings = model.encode([text1, text2], convert_to_tensor=True)
